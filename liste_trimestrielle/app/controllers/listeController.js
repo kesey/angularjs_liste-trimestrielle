62dda104b3398bb0cfcdd3604a4cbeb4b66da528
unknown <learning1@PC0112.acensi.local>
2015-08-12 10:31:18 +0200

    ajout de commentaires dans le controller et la vue home.html

--- liste_trimestrielle/app/controllers/listeController.js
+++ liste_trimestrielle/app/controllers/listeController.js
@@ -2,34 +2,35 @@ app
     .controller('listeController', ['$scope', function($scope){
         var monthNames = [ "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"],
 
-            firstQuarter = monthNames.slice(0, 3),
+            firstQuarter = monthNames.slice(0, 3),  // récupération des noms des mois pour chaque trimestre
             secondQuarter = monthNames.slice(3, 6),
             thirdQuarter = monthNames.slice(6, 9),
             fourthQuarter = monthNames.slice(9, 12),
 
             objDate = new Date();
 
-        function formatDate(value){
+        function formatDate(value){ // fonction permettant de formater la date sous la forme d/M/y
             return value.getDate() + "/" + (value.getMonth()+1) + "/" + value.getFullYear();
         }
 
-        $scope.date = formatDate(objDate);
+        $scope.date = formatDate(objDate); // récupération de la date du jour
 
-        $scope.visibility = 10;
+        $scope.visibility = 10; // initialisation de la visibilité sur 10 ans
 
-        $scope.fillOptions = function(){
-            var actualYear = parseInt($scope.date.split('/')[2]),
-                actualMonth = monthNames[parseInt($scope.date.split('/')[1])-1],
+        $scope.fillOptions = function(){ // fonction permettant de créer et remplir le tableau options sur lequel on itère dans la vue pour créer la liste dérouolante
+
+            var actualYear = parseInt($scope.date.split('/')[2]), // récupération de l'année actuelle
+                actualMonth = monthNames[parseInt($scope.date.split('/')[1])-1], // récupération du mois en cours
 
                 annees = [];
 
-            for(var i = actualYear; i <= actualYear + $scope.visibility; i++){
+            for(var i = actualYear; i <= actualYear + $scope.visibility; i++){ // on remplit le tableau annees permettant d'avoir une visibilité depuis l'année en cours jusqu'à un nombre N d'années (N étant définit dans $scope.visibility)
                 annees.push(i);
             }
 
             $scope.options = [];
 
-            for(var i = 0; i <= $scope.visibility; i++){
+            for(var i = 0; i <= $scope.visibility; i++){ // on remplit le tableau $scope.options en prenant soin de ne pas ajouter le trimestre en cours si on est dans le dernier mois du trimestre
                 if(actualMonth === firstQuarter[2] && actualYear === annees[i]){
                     var obj2 = {year: annees[i], quarter: secondQuarter},
                         obj3 = {year: annees[i], quarter: thirdQuarter},
